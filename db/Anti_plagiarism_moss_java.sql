3a5bbc22b10b4ba8e0ce13367d38c43c1bc57b48
jhsrcmh <jhsrcmh1992@gmail.com>
2012-11-02 12:31:10 +0800

    2012.11.2

---
+++ db/Anti_plagiarism_moss_java.sql
@@ -0,0 +1,33 @@
+﻿/*
+SQLyog Community Edition- MySQL GUI v6.04
+Host - 5.5.8-log : Database - anti-plagiarism
+*********************************************************************
+Server version : 5.5.8-log
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `anti-plagiarism`;
+
+USE `anti-plagiarism`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `result` */
+
+DROP TABLE IF EXISTS `result`;
+
+CREATE TABLE `result` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileA` varchar(100) DEFAULT NULL,
+  `fileB` varchar(100) DEFAULT NULL,
+  `score` float DEFAULT NULL,
+  `sameLines` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=301 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
